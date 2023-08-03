0a3a0c59a05a6471c58815f63f3597ef96fb081a
nana <nanajanashia@macbook.local>
2023-08-03 19:04:04 +0200

    remove assume role

--- main.tf
+++ main.tf
@@ -2,10 +2,6 @@ provider "aws" {
   region     = var.aws_region
   access_key = var.aws_access_key_id
   secret_key = var.aws_secret_access_key
-  
-  assume_role {
-    role_arn = "arn:aws:iam::696460314427:role/iac-role"
-  }
 
   default_tags {
     tags = {
