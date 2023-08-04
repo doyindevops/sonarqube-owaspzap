cea7649b651df8a17cbd4d4d135c970d060970da
Nana Janashia <nana.janashia@nnsoftware.at>
2023-08-04 12:20:53 +0000

    Update 2 files
    - /.gitlab-ci.yml
    - /providers.tf

--- providers.tf
+++ providers.tf
@@ -1,9 +1,4 @@
 terraform {
-  backend "s3" {
-    bucket = "infra-bucket-10"
-    key = "infra/state.tfstate"
-    region = "eu-west-3"
-  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
