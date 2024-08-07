52ba9aa71158dc1c3ea7a510bc3d66a0a207cd16
doyindevops <adedoyindevops@gmail.com>
2024-08-07 06:21:06 +0100

    troubleshoot

--- providers.tf
+++ providers.tf
@@ -3,7 +3,7 @@ terraform {
     bucket = "infra-bucket-devsecops"
     key = "infra/state.tfstate"
     region = "eu-west-3"
-    role = "aws_iam_role.github-runner-role.name"
+    
   }
   required_providers {
     aws = {
