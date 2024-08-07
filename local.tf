56f4ec7c7b7e581456bb2eec5003990b1ff759b2
doyindevops <adedoyindevops@gmail.com>
2024-08-07 05:20:59 +0100

    troubleshooting

---
+++ local.tf
@@ -0,0 +1,17 @@
+locals {
+  script = templatefile("${path.module}/scripts/script.tpl.sh", {})
+
+  script_github = templatefile("${path.module}/scripts/script-github.sh", {
+    runner_registration_token = var.runner_registration_token
+    github_repo = var.github_repo
+  })
+}
+
+output "script" {
+  value = local.script
+}
+
+output "script-github" {
+  value = local.script_github
+  sensitive = true 
+}
